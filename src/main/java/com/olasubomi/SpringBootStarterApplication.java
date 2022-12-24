b34b48f19bdadf6fbdd4228e0f93a0cd1189d4db
Olasubomi <shubbynlaa@gmail.com>
2022-12-24 15:56:00 +0100

    Learnt how to use JSON and records in Spring-boot.

--- src/main/java/com/olasubomi/SpringBootStarterApplication.java
+++ src/main/java/com/olasubomi/SpringBootStarterApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class SpringBootStarterApplication {
@@ -18,4 +20,12 @@ public class SpringBootStarterApplication {
 		return "Hi Soft";
 	}
 
+	@GetMapping("/greet")
+	public GreetResponse grit(){
+		GreetResponse greetResponse = new GreetResponse("Hi, Mr Soft", List.of("java", "python", "javascript"), new Person("Adewale", 30, 20000));
+		return greetResponse;
+	}
+
+	record GreetResponse(String greet, List<String> favProgrammingLanguage, Person person){}
+	record Person(String name, int age, double savings){}
 }
