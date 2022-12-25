2e57ea8af8a6822870b8ff5c4b7942a996b320da
Olasubomi <shubbynlaa@gmail.com>
2022-12-25 15:19:26 +0100

    Created a get request route to get the customers from the database.

--- src/main/java/com/olasubomi/Customer.java
+++ src/main/java/com/olasubomi/Customer.java
@@ -1,27 +1,24 @@
 package com.olasubomi;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jdk.jfr.Enabled;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
-@Enabled
+@Entity
 public class Customer {
     @Id
     @SequenceGenerator(
             name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
             generator = "customer_id_sequence"
     )
+    private Integer id;
     private String name;
     private String email;
-    private Integer id;
     private Integer age;
 
     public Customer(String name, String email, Integer id, Integer age) {
--- src/main/java/com/olasubomi/SpringBootStarterApplication.java
+++ src/main/java/com/olasubomi/SpringBootStarterApplication.java
@@ -3,16 +3,29 @@ package com.olasubomi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class SpringBootStarterApplication {
+
+	private final CustomerRepository customerRepository;
+
+	public SpringBootStarterApplication(CustomerRepository customerRepository){
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Customer> getCustomers(){
+		return customerRepository.findAll();
 	}
 
 	@GetMapping("/")
